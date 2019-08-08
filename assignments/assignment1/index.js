c48a4305f8c3764e8d20ee89d34a704b4640f1a6
Alessandro Negrini <alessandro2.negrini@gmail.com>
2019-08-08 18:19:18 +0200

    Code for leaderRouter

--- assignments/assignment1/index.js
+++ assignments/assignment1/index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 // importing my routes
 const dishRouter = require('./routes/dishRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 // constants -- TRY TO CREATE AN AD HOC MODULE FOR CONSTANTS
 const hostname = 'localhost';
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 // mounting dishRouter
 app.use('/dishes', dishRouter);
 app.use('/promotions',promotionRouter);
+app.use('/leaders',leaderRouter);
 app.use(express.static(__dirname+'/public'))
 
 const server = http.createServer(app);
