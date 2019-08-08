609773fe7ed72587b9c244a02ad3d1168ad842e9
Alessandro Negrini <alessandro2.negrini@gmail.com>
2019-08-08 18:06:43 +0200

    First part of assignment1

---
+++ assignments/assignment1/index.js
@@ -0,0 +1,27 @@
+// importing core/third-party node modules
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// importing my routes
+const dishRouter = require('./routes/dishRouter');
+
+// constants -- TRY TO CREATE AN AD HOC MODULE FOR CONSTANTS
+const hostname = 'localhost';
+const port = '3000';
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+
+// mounting dishRouter
+app.use('/dishes', dishRouter);
+app.use(express.static(__dirname+'/public'))
+
+const server = http.createServer(app);
+
+server.listen(port,hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
