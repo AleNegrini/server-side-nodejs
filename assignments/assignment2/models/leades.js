6b33a8ef872bbd9449460ad576b44ef01a0dd20a
Alessandro Negrini <alessandro2.negrini@gmail.com>
2019-08-12 16:14:54 +0200

    Assignment 2 code for Promo

---
+++ assignments/assignment2/models/leades.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    feature: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema]
+},{
+    timestamps: true
+});
+
+var Dishes = mongoose.model('Dish',dishSchema);
+
+module.exports = Dishes;
