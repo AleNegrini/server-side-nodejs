3f8680631dffd9c8340b6d110e3fb1d20e6e6d78
Alessandro Negrini <alessandro2.negrini@gmail.com>
2019-08-13 21:23:09 +0200

    Basic Authentication Examples

--- conFusionServer/routes/promotionRouter.js
+++ conFusionServer/routes/promotionRouter.js
@@ -1,6 +1,9 @@
 // importing core/third-party node modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Promos = require('../models/promotions');
 
 // creating router
 const promotionRouter = express.Router();
@@ -9,46 +12,73 @@ promotionRouter.use(bodyParser.json());
 
 // Router configuration
 promotionRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 .get((req, res, next) => {
-    res.end('Will send all the promotions to you!');
+    Promos.find({})
+    .then((promos) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promos);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the promotion: '+ req.body.name + ' with details: '+req.body.description);
+    Promos.create(req.body)
+    .then((promo) => {
+        console.log("Promotion created", promo);
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promo);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req, res, next) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /promotions');
+    res.end('PUT operation not supported on /dishes');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all the promotions!');
+    Promos.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 
 promotionRouter.route('/:promoId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 .get((req, res, next) => {
-    res.end("Will get the promo with id " +req.params.promoId+ " to you!");
+    Promos.findById(req.params.promoId)
+    .then((promo) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promo);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end("POST not supported on /promotions/"+req.params.promoId);
 })
 .put((req, res, next) => {
-    res.write('Updating the promotion: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion: ' + req.body.name + 
-        ' with details: ' + req.body.description);
+    Promos.findByIdAndUpdate(req.params.promoId, {
+        $set: req.body
+    }, {new: true})
+    .then((promo) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promo);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .delete((req, res, next) => {
-    res.end("Will delete promo with id "+req.params.promoId);
+    Promos.findByIdAndRemove(req.params.promoId)
+    .then((promo) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promo);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 module.exports = promotionRouter;
